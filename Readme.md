358401dc7fb6b8caef47cea6e3169667dfff7655
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2024-08-13 22:35:56 +0500

    update upwork rss token setup details

--- Readme.md
+++ Readme.md
@@ -2,7 +2,6 @@
 
 Connect to Upwork, find the most relevant jobs based on your skills or keywords, and generate tailored proposals using the latest strategies and insights.
 
-
 **Live Link**
 [Upwork Genie - Live GPT](https://chatgpt.com/g/g-2ti56xXvK-upwork-lead-genie)
 
@@ -21,8 +20,6 @@ This GPT system not only saves time by automating the job search process but als
 
 Important Note: Upwork Genie is designed as a Proof of Concept (POC) and is NOT recommended for production environments due to potential security risks.
 
-
-
 **Live Link**
 [Upwork Genie - Live GPT](https://chatgpt.com/g/g-2ti56xXvK-upwork-lead-genie)
 
@@ -39,6 +36,14 @@ Important Note: Upwork Genie is designed as a Proof of Concept (POC) and is NOT 
 - Docker Compose
 
 ### Setup Instructions
+
+0. Get an Upwork RSS Feed URL and extract security_token, user_uid and org_uid . Add them to main.py at line 31 - 35:
+```
+    security_token = "&securityToken="
+    user_uid = "&userUid="
+    org_uid = "&orgUid="
+```
+
 1. Clone the Repository
 ```bash
 git clone [your-repository-url]
--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ from app.core.config import logger_config
 logger = logger_config(__name__)
 
 app = FastAPI(
-    servers=[
-        {
-            "url": "https://upwork-leads-gpt.graybush-f94f1a24.eastus.azurecontainerapps.io",
-            "description": "Testing server of the API"
-        }
-    ]
+    # servers=[
+    #     {
+    #         "url": "https://url.io",
+    #         "description": "Testing server of the API"
+    #     }
+    # ]
 )
 
 
@@ -27,9 +27,10 @@ def fetch_jobs(
     paging_param = f"paging=0%3B{count}"
     keyword_param = f"&q={q}"
     sort_param = f"&sort={sort}"
-    security_token = "&securityToken=06499586d8200e1366f17091870270eada22996b2eef0fce9ca20ed089da95ee1ad4c939ddce8610b98ea3b286c20732abd731218c783334c3c3d7fc0dd3d4c2"
-    user_uid = "&userUid=1334406853214445568"
-    org_uid = "&orgUid=1334406853222834177"
+    
+    security_token = "&securityToken="
+    user_uid = "&userUid="
+    org_uid = "&orgUid="
 
     url = f"{base_url}{paging_param}{keyword_param}{
         sort_param}{security_token}{user_uid}{org_uid}"
